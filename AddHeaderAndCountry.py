b05d80e4244c1b256bb4dbfdf0b64f3c8f282533
Sean coyne <scoyne2@kent.edu>
2017-09-28 14:31:54 -0500

    Format Fullname as Last and First

--- AddHeaderAndCountry.py
+++ AddHeaderAndCountry.py
@@ -8,16 +8,22 @@ tmp = open( filename+'_Output'+file_extension, 'w')
 orig = open(fullFileName, 'r')
 
 #Hardcode header and delimiter, if the file format changes... yikes!
-#assign a country code based on file name
-headers = ['Name', 'Age', 'State','ZipCode','Phone','Country']
+#Assign a country code based on file name
+headers = ['FirstName', 'LastName', 'Age', 'State','ZipCode','Phone','Country']
 delimiter = '|'
 countryCode = filename.rsplit('_')[1]
 
-#write back the file, and add the header
+#Write back the file, and add the header
 tmp.write(delimiter.join(headers) + '\n')
 for line in orig.readlines():
-    #append country code and delimiter to tnd of line
-    tmp.write(line.rstrip('\n') + delimiter + countryCode + '\n')
+    #Append country code and delimiter to end of line
+    #Format the full namecolumn to be FirstName and LastName
+    columns = line.split('|')
+    columns[0:1] = columns[0].split(',')
+    columns[0] = columns[0].capitalize()
+    columns[1] = columns[1].capitalize()
+    #Format and save line
+    tmp.write(delimiter.join(columns).rstrip('\n') + delimiter + countryCode + '\n')
 
 #Close the files
 orig.close()
