e6ceb0493ebb17ac456bda3836c015e2cd0d468f
Sean coyne <scoyne2@kent.edu>
2017-09-28 14:05:30 -0500

    Made it work
    Changed code to fix formatting and pull in file name

--- AddHeaderAndCountry.py
+++ AddHeaderAndCountry.py
@@ -1,17 +1,25 @@
 #Import file add a header, and based on the file name add a country code to the data
 import os
 
-#Hardcode header, if the file format changes... yikes!
+#Open the file for processing
+fullFileName = 'SampleFile_USA.txt'
+filename, file_extension = os.path.splitext(fullFileName)
+tmp = open( filename+'_Output'+file_extension, 'w')
+orig = open(fullFileName, 'r')
+
+#Hardcode header and delimiter, if the file format changes... yikes!
+#assign a country code based on file name
 headers = ['Name', 'Age', 'State','ZipCode','Phone','Country']
-filename = 'SampleFile_USA.txt'
-tmp = open('TMP', 'w')
-orig = open(filename, 'r')
-tmp.write('\t'.join(headers) + '\n')
+delimiter = '|'
+countryCode = filename.rsplit('_')[1]
+
+#write back the file, and add the header
+tmp.write(delimiter.join(headers) + '\n')
 for line in orig.readlines():
-    #this is where we will want to split the file name based on the underscore and grab the text from the right side
-    #of the underscore, hardcode for now
-    tmp.write(line) + "USA"
+    #append country code and delimiter to tnd of line
+    tmp.write(line.rstrip('\n') + delimiter + countryCode + '\n')
+
+#Close the files
 orig.close()
 tmp.close()
-os.remove(filename)
-os.rename('TMP', filename)
+
